b60bce74e97eacbdc890c38b937e74132ea2705a
Vlad Z <vzelmanov@thoughtworks.com>
2012-04-20 13:45:54 +0530

    Revert "(Liyuan&Andrew on view details) Changed the url mapping for view details so it matches other url structures and corrected invoiceNumber to id where necessary."
    This reverts commit 8a050ba0a198a1208680951c681cc0a86570a43c.

--- src/main/java/kabbadi/controller/InvoiceController.java
+++ src/main/java/kabbadi/controller/InvoiceController.java
@@ -57,10 +57,10 @@ public class InvoiceController {
         return modelAndView;
     }
 
-    @RequestMapping(value = "invoice/{id}", method = RequestMethod.GET)
-    public ModelAndView viewDetails(@PathVariable("id")Integer id) {
+    @RequestMapping(value = "invoice/{invoiceID}/view", method = RequestMethod.GET)
+    public ModelAndView viewDetails(@PathVariable("invoiceID")String invoiceID) {
         ModelAndView modelAndView = new ModelAndView("invoice/view");
-        Invoice invoice = invoiceService.get(id);
+        Invoice invoice = invoiceService.findBy(invoiceID);
         modelAndView.addObject("invoice", invoice);
         return modelAndView;
         }
