5113f159c74c5af3a457324be415d579746d1b68
RyanDurling <rdurling@thoughtworks.com>
2012-04-23 12:18:55 +0530

    (Ryan & Vlad) Changed home page to invoice/list, fixed relevant tests

--- src/main/java/kabbadi/controller/HomeController.java
+++ src/main/java/kabbadi/controller/HomeController.java
@@ -1,13 +1,10 @@
 package kabbadi.controller;
 
-import kabbadi.domain.Invoice;
-import kabbadi.domain.User;
 import kabbadi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -26,14 +23,9 @@ public class HomeController {
         modelAndView.addObject("invoices",invoices);
         return modelAndView;
     }
+    
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView homepage(@RequestParam(value = "username", defaultValue = "") String username) {
-        ModelAndView modelAndView = new ModelAndView("home");
-        if (!username.isEmpty()) {
-            User user = userService.getUser(username);
-            modelAndView.addObject("user", user)
-                    .addObject("username", username);
-        }
-        return modelAndView;
-    }
+    public String homepage() {
+        return "redirect:/invoice/list";
+     }
 }
--- src/test/functional/pages/HomePage.java
+++ src/test/functional/pages/HomePage.java
@@ -1,17 +1,15 @@
 package pages;
 
-import config.Configuration;
 import org.openqa.selenium.WebDriver;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 public class HomePage extends BasePage {
     public HomePage(WebDriver driver) {
         super(driver);
 
-        assertThat(driver.getTitle(), equalTo("Home Page | Kabbadi"));
+        assertThat(driver.getTitle(), equalTo("List Invoices | Kabbadi"));
     }
 
 
