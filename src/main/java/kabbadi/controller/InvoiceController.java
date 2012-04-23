7f204574c68a2303b894204b3e8b2dbf2ff24986
RyanDurling <rdurling@thoughtworks.com>
2012-04-23 14:34:40 +0530

    (Vlad & Ryan on Edit Invoice) Fixed (unbroke?) clicking the edit buttan)

--- src/main/java/kabbadi/controller/InvoiceController.java
+++ src/main/java/kabbadi/controller/InvoiceController.java
@@ -15,7 +15,7 @@ import java.util.Date;
 import java.util.List;
 
 @Controller
-public class InvoiceController {
+public class    InvoiceController {
 
     private final InvoiceService invoiceService;
 
@@ -44,7 +44,7 @@ public class InvoiceController {
     }
 
     @RequestMapping(value = "invoice/edit/{id}", method = RequestMethod.GET)
-    public ModelAndView edit(@PathVariable Integer id) {
+    public ModelAndView edit(@PathVariable("id") Integer id) {
         return new ModelAndView("invoice/edit", "invoice", invoiceService.get(id));
     }
 
