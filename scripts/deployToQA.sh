b5c86bea1ae529a32ee6d455132e5181a60ea03c
Vlad Z <vzelmanov@thoughtworks.com>
2012-04-24 09:28:19 +0530

    Fixed deploy scripts and env settings

--- scripts/deployToQA.sh
+++ scripts/deployToQA.sh
@@ -1,3 +1,3 @@
 /opt/gradle-1.0-rc-1/bin/gradle -P env=qa war
-scp ./build/libs/kabbadi.war twu@10.4.3.1:/opt/jetty/webapp
+scp ./build/libs/kabbadi.war twu@10.4.3.1:/opt/jetty/webapps
 ssh twu@10.4.3.1 /etc/init.d/jetty restart
