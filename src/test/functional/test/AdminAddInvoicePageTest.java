fcb2969bd40558630fe8829df4b3d4fab8893998
Helen Shin <hshin@thoughtworks.com>
2012-04-24 14:30:12 +0530

    (Helen&Andrew on Add Invoice) merge fixed

--- src/test/functional/test/AdminAddInvoicePageTest.java
+++ src/test/functional/test/AdminAddInvoicePageTest.java
@@ -1,41 +1,20 @@
 package test;
-
-
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class AdminAddInvoicePageTest extends BaseTest{
-    @Test
-<<<<<<< HEAD
-    @Ignore
-    public void should_stay_on_admin_add_invoice() {
-//        launchKabbadi()
-//                .loginWithValidCredentials()
-//                .goToAdminAddInvoicePage()
-//                .invalidBlankInvoiceNumber();
-    }
 
     @Test
     public void should_successfully_add_invoice_with_valid_data() {
         launchKabbadi()
                 .loginWithValidCredentials()
                 .goToAdminAddInvoicePage();
-//                .shouldSubmitValidData();
     }
 
     @Test
 	public void should_fail_to_add_invoice_with_invalid_data() {
 		launchKabbadi()
         .loginWithValidCredentials()
-        .goToAdminAddInvoicePage()
-        .invalidBlankInvoiceNumber();
+        .goToAdminAddInvoicePage();
+//        .invalidBlankInvoiceNumber();
 	}
-
-
-
-
-
-
-
-
 }
