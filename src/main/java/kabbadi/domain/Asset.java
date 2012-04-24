002118dd1e276c51063c97f1667f2754be657731
bulanlily <bulanlily@gmail.com>
2012-04-24 16:59:51 +0530

    (Saravanan & Yuanli on view IS view list)added asset model

---
+++ src/main/java/kabbadi/domain/Asset.java
@@ -0,0 +1,35 @@
+package kabbadi.domain;
+
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="Asset")
+@Getter
+@Setter
+public class Asset {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String assetNumber;
+    private String modelName;
+    private String registeredOffice;
+    private String serialNumber;
+    private String registeredTo;
+    private String status;
+    private Boolean isLeased;
+    private Date leasingExpiration;
+    private Date maintenanceExpiration;
+    private String leasingCompany;
+
+    @ManyToOne
+    @JoinColumn(name="INVOICE_NUMBER")
+    private Invoice invoice;
+}
