d490597b4a2b509270a8aa9a40069a0b4685c705
RyanDurling <rdurling@thoughtworks.com>
2012-04-26 11:05:17 +0530

    (Ryan & Bruno on Money) Overrided toString of Money to print as a money

---
+++ src/main/java/kabbadi/domain/Money.java
@@ -0,0 +1,43 @@
+package kabbadi.domain;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Money {
+    private BigDecimal amount;
+
+    public Money(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public boolean equalTo(Money money) {
+        return amount.equals(money.amount);
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public Money times(Money variable) {
+        return new Money(amount.multiply(variable.getAmount()));
+    }
+
+
+    public Money add(Money variable) {
+        return new Money(amount.add(variable.getAmount()));
+    }
+
+    public Money minus(Money variable) {
+        return new Money(amount.subtract(variable.getAmount()));
+    }
+
+    // TODO : Needs a closer look at BigDecimal
+    public Money divideBy(Money variable) {
+        return new Money(amount.divide(variable.getAmount()));
+    }
+
+    @Override
+    public String toString() {
+        return amount.setScale(2, RoundingMode.HALF_EVEN).toString();
+    }
+}
