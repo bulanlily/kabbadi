1afc19ca393663b898934abbd4ac3988171a5fda
artherg <artherg@thoughtworks.com>
2012-05-04 17:11:50 +0530

    (Arther & DongDeng on Report Generation)Changed Location to Enum With Updated Tests

---
+++ src/main/java/kabbadi/domain/Location.java
@@ -0,0 +1,11 @@
+package kabbadi.domain;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: artherg
+ * Date: 04/05/12
+ * Time: 11:52 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Location {
+}
